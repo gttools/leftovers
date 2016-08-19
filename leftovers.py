45b37b8881e758eb74bb74b5f26dd031e3c7d3c2
Joshua Morton <joshua.morton13@gmail.com>
2016-08-18 22:55:12 -0400

    Added missing tests.

--- leftovers.py
+++ leftovers.py
@@ -1,5 +1,8 @@
 from typing import Iterable, Union
 
+class NoChildException(Exception):
+    pass
+
 class Course(object):
     """
     Represents a course that a student might take.
@@ -11,6 +14,9 @@ class Course(object):
         return (isinstance(self, Course) and 
                 isinstance(other, Course) and self.name == other.name)
 
+    def __hash__(self):
+        return hash(self.name)
+
 class Requirement(object):
     def __init__(self, 
             items: Iterable[Union['Requirement', Course]],
@@ -21,19 +27,26 @@ class Requirement(object):
         `num` represents the number of the children that must be completed for
         the requirement to be satisfied.
         """
-        self.courses = {c for c in items if isinstance(c, Course)}
-        self.children = {r for r in items if isinstance(r, Requirement)}
+        self.courses = frozenset(c for c in items if isinstance(c, Course))
+        self.children = [r for r in items if isinstance(r, Requirement)]
         self.completed = set()  # completed courses
         self.num = num
 
     @property
     def satisfied(self):
-        if len(self.completed) < self.num:
-            return False
-        if not self.completed < self.courses: 
-            return False
-        for child in self.children:
-            if not child.satisfied:
-                return False
-        return True
+        return (len(self.completed & self.courses) + 
+                sum(1 for x in self.children if x.satisfied)) >= self.num
+
+    def complete(self, course: Course):
+        if course in self.courses:
+            self.completed.add(course)
+        else:
+            for child in self.children:
+                try:
+                    child.complete(course)
+                    break
+                except NoChildException:
+                    pass
+            else:
+                raise NoChildException
 
--- leftovers_test.py
+++ leftovers_test.py
@@ -13,5 +13,35 @@ class CourseTest(unittest.TestCase):
         self.assertNotEqual(c1, c2)
 
 class LeftoversTest(unittest.TestCase):
-    def testCourses(self):
-        pass
+    def setUp(self):
+        self.cs1331 = leftovers.Course("CS1331")
+        self.cs1301 = leftovers.Course("CS1301")
+        self.cs1332 = leftovers.Course("CS1332")
+
+    def testNoRequirementsNotMet(self):
+        reqs = leftovers.Requirement([self.cs1301, self.cs1331], 2)
+        self.assertFalse(reqs.satisfied)
+
+    def testSingleRequirementMet(self):
+        reqs = leftovers.Requirement([self.cs1301], 1)
+        reqs.complete(self.cs1301)
+        self.assertTrue(reqs.satisfied)
+
+    def testSomeReqsNotMet(self):
+        reqs = leftovers.Requirement([self.cs1301, self.cs1331], 2)
+        reqs.complete(self.cs1301)
+        self.assertFalse(reqs.satisfied)
+
+    def testRequirementsMet(self):
+        reqs = leftovers.Requirement([self.cs1301, self.cs1331], 2)
+        reqs.complete(self.cs1301)
+        reqs.complete(self.cs1331)
+        self.assertTrue(reqs.satisfied)
+    
+    def testNestedRequirementsMet(self):
+        reqs = leftovers.Requirement([self.cs1301, self.cs1331], 2)
+        bigreq = leftovers.Requirement([reqs, self.cs1332], 2)
+        bigreq.complete(self.cs1301)
+        bigreq.complete(self.cs1331)
+        bigreq.complete(self.cs1332)
+        self.assertTrue(bigreq.satisfied)
